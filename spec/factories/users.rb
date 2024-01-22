108de58e9ec4164ee5522ca50d160aedaf88435f
k-suke39 <pz.mozarla@gmail.com>
2024-01-22 22:52:26 +0900

    [add]モデルスペックの追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,11 @@
 
 FactoryBot.define do
   factory :user do
+    nickname { '山田太郎' }
+    sequence :email do |n|
+      "test#{n}@example.com"
+    end
+    password { '111111' }
+    password_confirmation { '111111' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,49 @@
 
 require 'rails_helper'
 
-RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  let(:nickname) { 'テスト太郎' }
+  let(:email) { 'test@example.com' }
+  let(:password) { '11111111' }
+  let(:user) { build(:user, nickname:, email:, password:, password_confirmation: password) }
+
+  describe '.first' do
+    before do
+      create(:user, nickname:, email:)
+    end
+
+    subject { described_class.first }
+    it '作成したUserを返す' do
+      expect(subject.nickname).to eq(nickname)
+      expect(subject.email).to eq(email)
+    end
+  end
+
+  describe 'validation' do
+    describe 'nickname' do
+      context '正常系' do
+        context 'nicknameが20文字以下の場合' do
+          let(:nickname) { 'a' * 20 }
+          it 'Userオブジェクトは有効である' do
+            expect(user.valid?).to be(true)
+          end
+        end
+      end
+      context '異常系' do
+        context 'nicknameが21文字以上の場合' do
+          let(:nickname) { 'a' * 21 }
+          it 'Userオブジェクトは無効である' do
+            expect(user.valid?).to be(false)
+          end
+        end
+
+        context 'nicknameが空欄の場合' do
+          let(:nickname) { ' ' }
+          it 'Userオブジェクトは無効である' do
+            expect(user.valid?).to be(false)
+          end
+        end
+      end
+    end
+  end
 end
