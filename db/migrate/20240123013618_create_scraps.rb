2431a52bd357140d5b2b5ba4442a077fc4b48f62
k-suke39 <pz.mozarla@gmail.com>
2024-01-23 10:45:27 +0900

    [fix] lint

--- db/migrate/20240123013618_create_scraps.rb
+++ db/migrate/20240123013618_create_scraps.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class CreateScraps < ActiveRecord::Migration[7.0]
   def change
     create_table :scraps do |t|
-      t.string :title, null: false 
-      t.string :content, null: false 
-      t.references :user, foreign_key: true, null: false 
+      t.string :title, null: false
+      t.string :content, null: false
+      t.references :user, foreign_key: true, null: false
       t.timestamps
     end
   end
